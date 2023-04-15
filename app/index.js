61fe07067a30d3714e473c14fe0daf68c11a0f40
Tish254 <tiegomseeraoscar295@gmail.com>
2023-04-15 21:14:17 +0300

    dev: added _layout.js and index.js

---
+++ app/index.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView} from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+import { COLORS, icons, images, SIZES} from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from "../components"
+
+const Home = () => {
+    const router = useRouter();
+    return (
+        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.light}}>
+            <Text>
+                Home
+            </Text>
+        </SafeAreaView>
+    )
+}
+
+export default Home;

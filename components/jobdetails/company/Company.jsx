e2a88308681ba837a5d2a24910e294a8ac08f677
Tish254 <tiegomseeraoscar295@gmail.com>
2023-04-18 02:30:18 +0300

    dev: adding the job details page

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 
 import styles from './company.style'
+import { icons } from '../../../constants'
+import { checkImageURL } from '../../../utils'
 
-const Company = () => {
+const Company = ({companyLogo, jobTitle, companyName, location }) => {
   return (
     <View>
       <Text>Company</Text>

4af8bc446943374ed73449c43a53b9b08c40d2e2
Tish254 <tiegomseeraoscar295@gmail.com>
2023-04-17 19:36:57 +0300

    dev: added all the required files created the folder structure and added a few features:

--- app/index.js
+++ app/index.js
@@ -1,19 +1,49 @@
-import { useState } from 'react';
-import { View, ScrollView, SafeAreaView} from 'react-native';
-import { Stack, useRouter } from 'expo-router';
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
 
-import { COLORS, icons, images, SIZES} from '../constants';
-import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from "../components"
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
-    const router = useRouter();
-    return (
-        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.light}}>
-            <Text>
-                Home
-            </Text>
-        </SafeAreaView>
-    )
-}
+  const router = useRouter();
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension={30} />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension={30 } />
+          ),
+          headerTitle: "",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+            style={{
+                flex: 1,
+                padding: SIZES.medium
+            }}
+        >
+            <Welcome />
+
+            <Popularjobs />
+
+            <Nearbyjobs />
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default Home;
